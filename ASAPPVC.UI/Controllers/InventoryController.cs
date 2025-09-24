52b2cf4a168b73d411b7604fabeb1a62951c4008
Entropy-3 <Nathanteixeira981@gmail.com>
2025-09-24 14:48:03 +0200

    Add product management features to InventoryController
    Updated InventoryController to include new product-related view paths and action methods. Removed the viewInventory method and renamed viewPart to viewPartInventory. Initialized ViewProductInventory.cshtml with a comment block for MVC setup guidance.

--- ASAPPVC.UI/Controllers/InventoryController.cs
+++ ASAPPVC.UI/Controllers/InventoryController.cs
@@ -4,9 +4,17 @@ namespace ASAPPVC.UI.Controllers
 {
     public class InventoryController : Controller
     {
+        //Part constants
         private const string AddPartPath = "~/Views/Inventory/Parts/AddPart.cshtml";
         private const string ViewPartPath = "~/Views/Inventory/Parts/ViewPart.cshtml";
         private const string PartInventoryPath = "~/Views/Inventory/Parts/ViewPartInventory.cshtml";
+
+        //Product constants
+        private const string AddProductPath = "~/Views/Inventory/Products/AddProduct.cshtml";
+        private const string ViewProductPath = "~/Views/Inventory/Products/ViewProduct.cshtml";
+        private const string ProductInventoryPath = "~/Views/Inventory/Products/ViewProductInventory.cshtml";
+
+
         public IActionResult AddPart()
         {
             return View(AddPartPath);
@@ -15,9 +23,24 @@ namespace ASAPPVC.UI.Controllers
         {
             return View(ViewPartPath);
         }
-        public IActionResult viewInventory()
+        public IActionResult viewPartInventory()
         {
             return View(PartInventoryPath);
+        }
+
+        public IActionResult AddProduct()
+        {
+            return View(AddProductPath);
+        }
+
+        public IActionResult viewProduct()
+        {
+            return View(ViewProductPath);
+        }
+
+        public IActionResult viewProductInventory()
+        {
+            return View(ProductInventoryPath);
         }
     }
 }
