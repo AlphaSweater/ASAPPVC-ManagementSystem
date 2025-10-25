0b9818efd65e0faf7be8128eeb0c90f6016f0cf0
Entropy-3 <Nathanteixeira981@gmail.com>
2025-10-25 13:38:52 +0200

    Add StockTake features and product form
    Updated project file to include a new folder for stock taking views. Added `StockTakeController` with an `Index` action method. Created `AddProductPartial.cshtml` for a product addition form, including image upload and product details input.

---
+++ ASAPPVC.UI/Controllers/StockTakeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASAPPVC.UI.Controllers
+{
+    public class StockTakeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
